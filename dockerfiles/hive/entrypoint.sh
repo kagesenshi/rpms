a641f9bacd3d0201915c8f28e8bbe5b3ff26c9c5
Izhar Firdaus <kagesenshi.87@gmail.com>
2022-06-26 02:14:29 +0800

    fix environment vars

--- dockerfiles/hive/entrypoint.sh
+++ dockerfiles/hive/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 if [ "$1" == "schematool" ];then
-    /opt/apache/hive/bin/hive --service schemaTool "${@:2}"
+    /opt/apache/hive/bin/hive --service schemaTool -dbType ${DB_TYPE} -validate
+    if [ "x$?"  != "x0" ];then
+        /opt/apache/hive/bin/hive --service schemaTool -dbType ${DB_TYPE} "${@:2}"
+    fi
 elif [ "$1" == "metastore" ];then
     /opt/apache/hive/bin/hive --service metastore "${@:2}"
 elif [ "$1" == "noop" ];then
