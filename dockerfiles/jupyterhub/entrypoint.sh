3fd5548728d3574f365963b374cdd17754f5c1f8
Izhar Firdaus <kagesenshi.87@gmail.com>
2022-08-10 20:00:55 +0800

    added patch to fix pod hostname

--- dockerfiles/jupyterhub/entrypoint.sh
+++ dockerfiles/jupyterhub/entrypoint.sh
@@ -6,9 +6,16 @@ TOKEN=${JUPYTER_LAB_TOKEN-''}
 if [ "$1" == "debug" ];then
     tail -f /dev/null
 elif [ "$1" == "hub" ];then
-    jupyterhub
+    jupyterhub -f /etc/jupyterhub/jupyterhub_config.py
 elif [ "$1" == "lab" ];then
     /opt/jupyterhub/bin/jupyter lab --ip 0.0.0.0 --port 8000 --NotebookApp.token="${TOKEN}"
+elif [ "$1" == "jupyterhub-singleuser" ];then
+    export K8S_POD_NAME=`hostname -s`
+    userdel -f jupyter
+    groupadd ${JUPYTERHUB_USER} -g 1000
+    useradd ${JUPYTERHUB_USER} -u 1000 -g 1000
+    cd /home/${JUPYTERHUB_USER}
+    sudo -E -u ${JUPYTERHUB_USER} -- "$@"
 else
     "$@"
 fi
