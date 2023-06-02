ee8e10f930e2d9ce7715cab85d1962ee72113a08
Izhar Firdaus <kagesenshi.87@gmail.com>
2023-06-02 08:56:24 +0800

    'interface' renamed to 'conn'

--- setup-node.sh
+++ setup-node.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-PARSED=$(getopt -a -n setup-node -o :n:i:g:d:I: --long name:,ip: -- "$@")
+PARSED=$(getopt -a -n setup-node -o :n:i:g:d:c: --long name:,ip:,gateway:,dns:,connection: -- "$@")
 VALID_ARGS=$?
 
 usage() {
-   echo "Usage: $0 -n [NAME] -i [IP] -g [GATEWAY] -d [DNS]"
+   echo "Usage: $0 -n [NAME] -i [IP] -g [GATEWAY] -d [DNS] -c [NMCONN]"
    exit 1
 }
 
@@ -21,7 +21,7 @@ do
      -i | --ip) IP="$2"; shift 2 ;;
      -g | --gateway) GATEWAY="$2"; shift 2;;
      -d | --dns) DNS="$2"; shift 2;;
-     -I | --interface) INTERFACE="$2"; shift 2;;
+     -c | --connection) CONN="$2"; shift 2;;
      --) shift; break;;
      *) echo "Invalid option $1"; usage;;
 
@@ -44,16 +44,16 @@ if [ "x$DNS" == "x" ];then
    usage
 fi
 
-if [ "x$INTERFACE" == "x" ];then
+if [ "x$CONN" == "x" ];then
    usage
 fi
 
 set -x
 
-nmcli con mod ${INTERFACE} ipv4.addresses ${IP}/24
-nmcli con mod ${INTERFACE} ipv4.gateway ${GATEWAY}
-nmcli con mod ${INTERFACE} ipv4.dns ${DNS}
-nmcli con mod ${INTERFACE} ipv4.method manual
+nmcli con mod "${CONN}" ipv4.addresses ${IP}/24
+nmcli con mod "${CONN}" ipv4.gateway ${GATEWAY}
+nmcli con mod "${CONN}" ipv4.dns ${DNS}
+nmcli con mod "${CONN}" ipv4.method manual
 
 cat << EOF > /etc/NetworkManager/conf.d/99-dns-none.conf
 [main]
@@ -65,5 +65,5 @@ nameserver ${DNS}
 EOF
 
 echo ${NAME}.${IP}.sslip.io > /etc/hostname
-nmcli con up enp1s0
+nmcli con up "${CONN}"
 hostname `cat /etc/hostname`
